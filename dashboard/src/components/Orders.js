557f7cd5ac949d6bc95302413961ae4a1efd11a6
Alok Patel <alokpatel23273@gmail.com>
2025-04-09 02:41:17 +0530

    commites

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -1,17 +1,75 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { VerticalGraph } from "./VerticalGraph";
 
 const Orders = () => {
-  return (
-    <div className="orders">
-      <div className="no-orders">
-        <p>You haven't placed any orders today</p>
+  const [allOrders, setAllOrders] = useState([]);
 
-        <Link to={"/"} className="btn">
-          Get started
-        </Link>
+  useEffect(() => {
+    axios.get("http://localhost:3002/allOrders").then((res) => {
+      setAllOrders(res.data);
+    });
+  }, []);
+
+  const labels = allOrders.map((subArray) => subArray["name"]);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Stock Price",
+        data: allOrders.map((stock) => stock.price),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <>
+      <h3 className="title">Orders ({allOrders.length})</h3>
+
+      <div className="order-table">
+        <table>
+          <tr>
+            <th>Instrument</th>
+            <th>Qty.</th>
+            <th>Price</th>
+            <th>mode</th>
+          </tr>
+
+          {allOrders.map((stock, index) => {
+            return (
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.price}</td>
+                <td>{stock.mode}</td>
+              </tr>
+            );
+          })}
+        </table>
       </div>
-    </div>
+
+      <div className="row">
+        <div className="col">
+          <h5>
+            29,875.<span>55</span>{" "}
+          </h5>
+          <p>Total investment</p>
+        </div>
+        <div className="col">
+          <h5>
+            31,428.<span>95</span>{" "}
+          </h5>
+          <p>Current value</p>
+        </div>
+        <div className="col">
+          <h5>1,553.40 (+5.20%)</h5>
+          <p>P&L</p>
+        </div>
+      </div>
+      <VerticalGraph data={data} />
+    </>
   );
 };
 
